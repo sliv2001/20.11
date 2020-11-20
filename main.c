e37200064be99b2eff3af724112866435772cae1
sliv2001 <ivan.sladkov@yandex.ru>
2020-11-20 09:54:51 +0300

    No . & .. dirs

--- main.c
+++ main.c
@@ -1,15 +1,20 @@
 #include <dirent.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char** argv){
 	char* fullpath;
 	struct dirent *dirp;
 	size_t pathlen;
-	DIR* d = opendir(argv[1]);
+	DIR* d;
 	if (argc!=2)
 		return -1;
+	d = opendir(argv[1]);
+	if (d == NULL)
+		return -1;
 	while((dirp=readdir(d))!=NULL){
-		printf("%s\n", dirp->d_name);
+		if (strcmp(".", dirp->d_name)!=0&&strcmp("..", dirp->d_name)!=0)
+			printf("%s\n", dirp->d_name);
 	}
 	closedir(d);
 	return 0;
